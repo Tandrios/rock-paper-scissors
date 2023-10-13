e6cfa94b85b1c2180745edf9f724435840220e9c
Tandrios <t.andriessen@gmail.com>
2023-10-13 20:36:48 +0200

    V2

---
+++ script.js
@@ -0,0 +1,88 @@
+addEventListener("DOMContentLoaded", (event) => {
+
+    // When button is clicked update <p> output-self
+        //Get buttons
+        const buttons = document.querySelectorAll('button');
+        
+
+        // Add event listener on each button and check button id => update html with correct one
+        buttons.forEach(function (button) {
+            button.addEventListener('click', function (event) {
+                console.log(event.target.id);
+                switch (event.target.id) {
+                    case "rock":
+                        document.querySelector("#output-self").innerHTML = this.innerHTML;
+                        printWinner(winner(event.target.id, computerChoice())); //printWinner prints the winner which is the return value of winner, which takes 2 inputs.
+                        break;
+                    case "paper":
+                        document.querySelector("#output-self").innerHTML = this.innerHTML;
+                        printWinner(winner(event.target.id, computerChoice()))
+                        break;
+                    case "scissors":
+                        document.querySelector("#output-self").innerHTML = this.innerHTML;
+                        printWinner(winner(event.target.id, computerChoice()))
+                        break;
+                }
+            });
+        });
+
+
+
+});
+
+// Function to determine choice computer, returns value
+function computerChoice (){
+    computerInt = Math.floor(Math.random() * 3);
+
+    switch (computerInt) {
+        case 0:
+            document.querySelector("#output-computer").innerHTML = "Rock";
+            return "rock";
+        case 1:
+            document.querySelector("#output-computer").innerHTML = "Paper";
+            return "paper"
+        case 2:
+            document.querySelector("#output-computer").innerHTML = "Scissors";
+            return "scissors"
+    }
+}
+
+//Function to determine winner
+function winner (self, computer) {
+    if (self === 'rock') {
+        if (computer === 'rock') {
+            return "tie";
+        } else if (computer === "paper") {
+            return "computer";
+        } else if (computer === "scissors") {
+            return "self"
+        }
+    } else if (self === "paper") {
+        if (computer === 'rock') {
+            return "self";
+        } else if (computer === "paper") {
+            return "tie";
+        } else if (computer === "scissors") {
+            return "computer"
+        }
+    } else if (self === "scissors") {
+        if (computer === 'rock') {
+            return "computer";
+        } else if (computer === "paper") {
+            return "self";
+        } else if (computer === "scissors") {
+            return "tie"
+        }
+    }
+    
+};
+
+function printWinner (winner) {
+    if (winner === "self") {
+        document.querySelector("#winner").innerHTML = "You've won!"
+    } else if (winner === "computer") {
+        document.querySelector("#winner").innerHTML = "Computer wins!"
+    } else {
+        document.querySelector("#winner").innerHTML = "It's a tie!"
+    }
+}
